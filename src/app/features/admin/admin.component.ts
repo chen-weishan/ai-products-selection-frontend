61ced7ed820b482ae6876a3bf887a63da5bcf6e6
yoyo <yenyoyo930418@gmail.com>
2026-08-19 16:52:02 +0800

    dialog
    寫了全域dialog，有時頁面會發生錯誤，目前找不到原因

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component,inject } from '@angular/core';
+import{MatButtonModule}from '@angular/material/button';
+import{DialogService}from '../../services/dialog-service';
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  private  readonly dialogService = inject(DialogService);
 
+  testNormalDialog(): void {
+    this.dialogService.Confirm({
+      'title': '系統通知確認',
+      'message': '這是一般確認視窗，您確定要繼續執行嗎？',
+      'confirmText':'確認執行',
+      'cancelText':'取消',
+      'isDanger': false
+    }).subscribe(result => {
+      console.log('一般彈窗回傳結果：', result);
+    });
+  }
+
+  // 3. 測試危險操作
+  testDangerDialog(): void {
+    this.dialogService.Confirm({
+      'title': '刪除管理員警告',
+      'message': '此操作將移除該管理員帳號與所有授權權限，確認要刪除嗎？',
+      'confirmText': '強制刪除',
+      'cancelText':'返回',
+      'isDanger': true
+    }).subscribe(result => {
+      console.log('危險彈窗回傳結果：', result);
+    });
+  }
 }
