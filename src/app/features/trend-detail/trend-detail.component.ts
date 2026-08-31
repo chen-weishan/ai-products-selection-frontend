aad1ccf6912e6ad825ff0bc4b6f381a1d116be9f
yoyo <yenyoyo930418@gmail.com>
2026-08-31 17:36:45 +0800

    chart 1/2
    做完曲線圖的一半

--- src/app/features/trend-detail/trend-detail.component.ts
+++ src/app/features/trend-detail/trend-detail.component.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, signal, DestroyRef, ElementRef, ViewChild } from '@angular/core';
+import { TrendControllerService, TrendKeywordDetailResponse } from '../../api';
+import { ActivatedRoute, Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-trend-detail',
   imports: [],
   templateUrl: './trend-detail.component.html',
   styleUrl: './trend-detail.component.scss',
 })
-export class TrendDetailComponent {}
+export class TrendDetailComponent {
+  @ViewChild('chartCanvas') chartCanvas!: ElementRef<HTMLCanvasElement>;
+  private route = inject(ActivatedRoute);
+  private readonly trendService = inject(TrendControllerService)
+  private readonly router = inject(Router)
+  private readonly destroyRef = inject(DestroyRef)
+  isLoading = signal(false)
+  errorMessage = signal<string | null>(null)
+  trendData = signal<TrendKeywordDetailResponse | null>(null)
+
+  ngOnInit(): void {
+    const rawid = this.route.snapshot.paramMap.get('keywordId')
+    const keywordId = Number(rawid)
+    if (!rawid || isNaN(keywordId)) {
+      this.errorMessage.set('無效關鍵字ID');
+      return;
+    }
+    this.loadTrendDetail(keywordId);
+
+  }
+
+  loadTrendDetail(keywordId: number) {
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+    this.trendService.getKeywordDetail({ keywordId }).
+      pipe(takeUntilDestroyed(this.destroyRef)).
+      subscribe({
+        next: (res) => {
+          this.trendData.set(res);
+          this.isLoading.set(false);
+        },
+        error: (err) => {
+          this.errorMessage.set('載入失敗,請稍後再嘗試!');
+          this.isLoading.set(false);
+        }
+      })
+  }
+
+  goBack(): void {
+    this.router.navigate(['/trends']);
+  }
+
+  DataSetting: string[] = [
+    '近90天', '近60天', '近30天'
+  ]
+
+
+
+}
